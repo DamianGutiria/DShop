f0f82bbc9ae5b8d999fce9928e5993f7826231f5
Damian Gutierrez Gaviria <damian41777@gmail.com>
2022-10-12 09:04:32 -0500

    Update productsController.js
    creando consultas

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,18 +1,27 @@
 const producto = require("../models/products")
 
 //ver la lista de productos
-exports.getProducts = (req, res, next) => {
-    res.status(200).json({
-        sucess: true,
-        message: "En esta ruta se veran los productos"
-    })
+exports.getProducts = async(req, res, next) => {
+        const productos = await producto.find();
+        res.status(200).json({
+            sucess: true,
+            count: productos.length,
+            productos
+        })
+    }
+    // consulta de solo un objeto 
+
+exports.getProductById = async(req, res, next) => {
+
 }
+
 
 //Crear nuevo producto /api/productos
 
 exports.newProduct = async(req, res, next) => {
     const product = await producto.create(req.body);
     res.status(201).json({
-
+        sucess: true,
+        pruduct
     })
 }
