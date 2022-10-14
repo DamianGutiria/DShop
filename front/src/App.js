5a0456a2556033d6cc8d9eccceeeedc75e655ed5
Damian Gutierrez Gaviria <damian41777@gmail.com>
2022-10-14 00:55:46 -0500

    front clase 7
    creación del front 1

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        h1 > Tienda de BMX freestyle < /h1>  <
+        img src = './images/logo512.png'
+        alt = 'logo' / >
+        <
+        /div >
+    );
+}
+
+export default App;
